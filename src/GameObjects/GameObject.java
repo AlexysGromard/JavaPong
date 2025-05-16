537200a52181f6c836b5a107756d618d65a8ece5
Mathis RABUSSEAU <mathis.rabusseau@etu.univ-nantes.fr>
2025-05-16 10:21:16 +0200

    Classe  abstraite GameObject

--- src/GameObjects/GameObject.java
+++ src/GameObjects/GameObject.java
@@ -1,6 +1,19 @@
 package src.GameObjects;
 
+import src.utils.Vector2;
+
 public abstract class GameObject {
+
     
+    String name;
+    Vector2 position;
+
+    
+    void onCollisionEnter(GameObject collision){}
+
+    void update(){};
+
+    void  init(){};
+
     
 }
--- src/windows/Window.java
+++ src/windows/Window.java
@@ -1,6 +1,5 @@
 package src.windows;
 import javax.swing.*;
-import java.awt.*;
 
 public class Window  extends JFrame{
     
