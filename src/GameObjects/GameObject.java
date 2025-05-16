fd76b2bbe71035d76ec99be92fefb1fe81b25f7d
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-05-16 11:20:24 +0200

    Ajout de la méthode draw

--- src/GameObjects/GameObject.java
+++ src/GameObjects/GameObject.java
@@ -4,19 +4,28 @@ import utils.Vector2;
 
 public abstract class GameObject {
 
-    
     public String name;
     public Vector2 position;
-
     public int width;
     public int height;
 
-    
+    /**
+     * Calculates if the object is colliding with another object
+     * @param collision The other object to check collision with
+     */
     public void onCollisionEnter(GameObject collision){}
 
+    /**
+     * Called each frame to update the object
+     * @param g Graphics object to draw on
+     */
     public void update(Graphics g){};
 
-    public void init(){};
+    /**
+     * Draws the object on the screen
+     * @param g Graphics object to draw on
+     */
+    public void draw(Graphics g){}
 
     
 }
--- src/GameObjects/objects/Paddle.java
+++ src/GameObjects/objects/Paddle.java
@@ -3,4 +3,11 @@ package GameObjects.objects;
 import GameObjects.GameObject;
 
 public class Paddle extends GameObject {
+    public Paddle(String name, int x, int y, int width, int height) {
+        this.name = name;
+        this.position.x = x;
+        this.position.y = y;
+        this.width = width;
+        this.height = height;
+    }
 }
