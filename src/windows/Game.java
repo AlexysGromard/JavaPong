f7fe3720218202af8d66e1d5cb878f9efc60bdf8
Mathis RABUSSEAU <mathis.rabusseau@etu.univ-nantes.fr>
2025-05-16 11:23:08 +0200

    Ajoute les bords gauches et droits

--- src/windows/Game.java
+++ src/windows/Game.java
@@ -4,7 +4,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JPanel;
 import GameObjects.GameObject;
 import GameObjects.objects.Border;
