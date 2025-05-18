90a1ce37a956bc0feed586a940fac87661615d32
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-05-18 15:36:14 +0200

    Création des bordures

--- src/GameObjects/objects/Paddle.java
+++ src/GameObjects/objects/Paddle.java
@@ -25,7 +25,7 @@ public class Paddle extends GameObject {
 
         // Draw the shadow
         Color shadowColor = new Color(RGBColor.getRed(), RGBColor.getGreen(), RGBColor.getBlue(), 25);
-        for (int i = 8; i >= 1; i--) { // Spread simulated by fuzzy concentric circles
+        for (int i = 12; i >= 1; i--) { // Spread simulated by fuzzy concentric circles
             g2.setColor(new Color(shadowColor.getRed(), shadowColor.getGreen(), shadowColor.getBlue(), 64 / i)); // Lighter as we move away
             g2.fillRoundRect(position.x - i, position.y - i,
                     width + 2 * i, height + 2 * i,
--- src/windows/Game.java
+++ src/windows/Game.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JPanel;
 import GameObjects.GameObject;
+import GameObjects.objects.Border;
 import GameObjects.objects.Paddle;
 import utils.FontManager;
 
@@ -20,17 +21,29 @@ public class Game extends JPanel {
         this.InstantiateObjects();
     }
 
+    /**
+     * Instantiates the game objects.
+     * This method creates the borders, paddles, puck, and texts.
+     * It is called in the constructor of the Game class.
+     */
     private void InstantiateObjects(){
+        // Create the borders
+        gameObjects.add(new GameObjects.objects.Border("Border_left", new utils.Vector2(0, 0), new utils.Vector2(6, 1024), new Color(0, 255, 247, 50), new Color(0, 255, 247), Border.BorderType.RIGHT));
+        gameObjects.add(new GameObjects.objects.Border("Border_right", new utils.Vector2(1434, 0), new utils.Vector2(6, 1024), new Color(255, 0, 224, 50), new Color(255, 0, 224), Border.BorderType.LEFT));
+        gameObjects.add(new GameObjects.objects.Border("Border_top", new utils.Vector2(0, 0), new utils.Vector2(1440, 6), new Color(51, 51, 51)));
+        gameObjects.add(new GameObjects.objects.Border("Border_bottom", new utils.Vector2(0, 1018), new utils.Vector2(1440, 6), new Color(51, 51, 51)));
+        gameObjects.add(new GameObjects.objects.Border("Border_center", new utils.Vector2(716, 0), new utils.Vector2(6, 1024), new Color(51, 51, 51)));
+
+        // Create the texts
+        gameObjects.add(new GameObjects.objects.Text("Score_left", 629, 27, "0", 64, FontManager.OrbitronStyle.MEDIUM, Color.WHITE));
+        gameObjects.add(new GameObjects.objects.Text("Score_right", 759, 27, "0", 64, FontManager.OrbitronStyle.MEDIUM, Color.WHITE));
+
         // Create the paddles
         gameObjects.add(new Paddle("Paddle_left", 37, 407, 12, 210, new Color(0, 255, 247)));
         gameObjects.add(new Paddle("Paddle_right", 1391, 407, 12, 210, new Color(255, 0, 224)));
 
         // Create the puck
         gameObjects.add(new GameObjects.objects.Puck("Puck", 700, 493, 39, 39));
-
-        // Create the texts
-        gameObjects.add(new GameObjects.objects.Text("Score_left", 629, 27, "0", 64, FontManager.OrbitronStyle.MEDIUM, Color.WHITE));
-        gameObjects.add(new GameObjects.objects.Text("Score_right", 759, 27, "0", 64, FontManager.OrbitronStyle.MEDIUM, Color.WHITE));
     }
 
     @Override
@@ -46,7 +59,7 @@ public class Game extends JPanel {
         // Calculating the scale factor
         double scaleX = getWidth() / (double) refWidth;
         double scaleY = getHeight() / (double) refHeight;
-        double scale = Math.min(scaleX, scaleY); // Pour garder les proportions
+        double scale = Math.min(scaleX, scaleY);
 
         // Calculating the offset to center
         int xOffset = (int) ((getWidth() - refWidth * scale) / 2);
