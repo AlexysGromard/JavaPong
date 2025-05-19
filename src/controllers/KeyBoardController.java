5eaf0732fd845ea5b06539a7130bc9124d9651f8
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-05-19 11:30:15 +0200

    Gestion du déplacement progressif des Paddles

--- src/controllers/KeyBoardController.java
+++ src/controllers/KeyBoardController.java
@@ -9,8 +9,11 @@ import java.awt.event.KeyEvent;
 public class KeyBoardController extends KeyAdapter implements PaddleController {
     private final int keyUpCode;
     private final int keyDownCode;
+
     private boolean isUpPressed = false;
     private boolean isDownPressed = false;
+    private double upPressTime = 0;
+    private double downPressTime = 0;
 
     private final int minY;
     private final int maxY;
@@ -44,15 +47,45 @@ public class KeyBoardController extends KeyAdapter implements PaddleController {
         }
     }
 
+    /**
+     * Easing function for acceleration.
+     * This function is used to create a smooth acceleration effect.
+     *
+     * @param t The time factor (0 to 1).
+     * @return The eased value.
+     */
+    private double ease(double t) {
+        return 1 - Math.pow(1 - t, 4);
+    }
+
     @Override
     public Vector2 computePosition(Vector2 position) {
         if (internalY == 0) internalY = position.y;
 
-        double speed = 720.0 / FPSCounter.getFPS(); // 720.0 pixels/second
-        double dy = 0;
+        double deltaTime = 1.0 / FPSCounter.getFPS();
 
-        if (isUpPressed) dy -= speed;
-        if (isDownPressed) dy += speed;
+        // Managing the time of key presses
+        if (isUpPressed) upPressTime += deltaTime;
+        else upPressTime = 0;
+
+        if (isDownPressed) downPressTime += deltaTime;
+        else downPressTime = 0;
+
+        // Calculating the speed
+        double dy = 0;
+        double baseSpeed = 720.0; // pixels/second max
+
+        if (isUpPressed) {
+            double t = Math.min(1.0, upPressTime);
+            double factor = ease(t);
+            dy -= baseSpeed * factor * deltaTime;
+        }
+
+        if (isDownPressed) {
+            double t = Math.min(1.0, downPressTime);
+            double factor = ease(t);
+            dy += baseSpeed * factor * deltaTime;
+        }
 
         internalY += dy;
 
@@ -62,6 +95,4 @@ public class KeyBoardController extends KeyAdapter implements PaddleController {
 
         return new Vector2(position.x, (int) internalY);
     }
-
-
 }
