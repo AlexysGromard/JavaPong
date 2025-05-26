90454474375efa326b6c43d83a5e89fffa101d0f
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-05-26 10:37:59 +0200

    Commentaires Vector2

--- src/utils/Vector2.java
+++ src/utils/Vector2.java
@@ -1,9 +1,20 @@
 package utils;
 
+/**
+ * The Vector2 class represents a 2D vector with integer coordinates.
+ * It provides a simple data structure to store and manage the X and Y components
+ * of a two-dimensional vector.
+ */
 public class Vector2 {
     public int x;
     public int y;
 
+    /**
+     * Constructs a new instance of the Vector2 class with specified x and y coordinates.
+     *
+     * @param x The X-coordinate of the 2D vector.
+     * @param y The Y-coordinate of the 2D vector.
+     */
     public Vector2(int x, int y){
         this.x = x;
         this.y = y;
